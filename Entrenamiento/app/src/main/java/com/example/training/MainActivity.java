e3564af16c9d0134de7365be0e1dedbb0781e528
braywolf11 <brayfer1893@gmail.com>
2020-03-04 21:54:45 -0500

    TOAST

--- Entrenamiento/app/src/main/java/com/example/training/MainActivity.java
+++ Entrenamiento/app/src/main/java/com/example/training/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.training;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Message;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "Hola nonorrea", Toast.LENGTH_SHORT).show();
+
     }
+
+    public void Message(View view){
+
+        Toast.makeText(this, "You`ve pressed button", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "on start", Toast.LENGTH_LONG).show();
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "En Pantalla", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "en pausa", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "EN stop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "La app se reinicio", Toast.LENGTH_SHORT).show();
+    }
+
+
 }
