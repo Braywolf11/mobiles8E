d95ae8504ea379ec44a8b2b6ca1e3eeb68c14eec
braywolf11 <brayfer1893@gmail.com>
2020-03-11 21:37:51 -0500

    clase

--- Market/app/src/main/java/com/example/market/LoginActivity.java
+++ Market/app/src/main/java/com/example/market/LoginActivity.java
@@ -2,7 +2,10 @@ package com.example.market;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void BtnLogin(View view){
+        //Toast.makeText(this, "you have pressed Login Button", Toast.LENGTH_SHORT).show();
+        Intent actHome = new Intent(this,HomeActivity.class);
+        startActivity(actHome);
+    }
+
+    public void BtnSingup(View view){
+        //Toast.makeText(this, "Sing up", Toast.LENGTH_SHORT).show();
     }
 }
